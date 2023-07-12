69468f1ce059db39bc371296a9d2e0ad09595c4c
Sreerag <sreeragatk3@gmail.com>
2023-07-12 20:21:11 +0530

    Create: Initial Project setup

---
+++ config/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+	DB_URL: 'mongodb+srv://sreeragatk3:dcrZEiTEvtP38jP6@cluster0.ttu4bsa.mongodb.net/ecommerce?retryWrites=true&w=majority'
+};
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+///require('dotenv').config();
+require('./startup/logging')();
+require('./services/database.services')();
+require('./startup/routes')(app);
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+	return res.send('SERVER STARTED..');
+});
+
+app.listen(PORT, () => console.log(`SERVER LISTENING ON PORT ${PORT}`));
